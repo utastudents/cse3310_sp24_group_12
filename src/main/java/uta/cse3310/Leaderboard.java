703d716307a55a031cc05ad141a3cc4f8e6e12b6
Amogh <axm0754@mavs.uta.edu>
2024-03-17 07:04:11 -0500

    Removed old code that needs replacing

--- src/main/java/uta/cse3310/Leaderboard.java
+++ src/main/java/uta/cse3310/Leaderboard.java
@@ -1,8 +1,5 @@
 package uta.cse3310;
 
 public class Leaderboard{
-
-    public void Leaderboard_data(PlayerType player, int score){
-        //code to be implemented later
-    }
+    // code to be implemented later
 }
