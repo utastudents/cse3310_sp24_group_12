795e9afd9ca9e5707982b0308aca07eaa3d5f3c8
Amogh <axm0754@mavs.uta.edu>
2024-03-17 07:04:49 -0500

    Constructor not required

--- src/main/java/uta/cse3310/UserEvent.java
+++ src/main/java/uta/cse3310/UserEvent.java
@@ -5,14 +5,4 @@ public class UserEvent {
     int GameId; 
     PlayerType PlayerId; 
     int button; 
-
-    UserEvent() {
-
-    }
-    //class constructor
-    UserEvent(int _GameId, PlayerType _PlayerId, int _button) {
-        GameId = _GameId;
-        PlayerIdx = _PlayerIdx;
-        Button = _Button;
-    }
 }
