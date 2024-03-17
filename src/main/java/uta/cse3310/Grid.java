bdfccb559969dd4eab3d849d13e1ae494f84a77b
Alexander Zacarias <113472783+Alex48821@users.noreply.github.com>
2024-03-17 15:25:59 -0500

    Added UML diagram specifications

--- src/main/java/uta/cse3310/Grid.java
+++ src/main/java/uta/cse3310/Grid.java
@@ -1,5 +1,16 @@
 package uta.cse3310;
 
 public class Grid {
+  //class atributes
+  private char[] grid;
+  private ArrayList<Word> words;
 
+  //methods
+  public void createGrid(){
+    //code to be implemented later
+  }
+
+  public void highlightWord(){
+    //code to be implemented later
+  }
 }
