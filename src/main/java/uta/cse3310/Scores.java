62efa4a3f1c641b906c5d4abebed81b816432ab5
Alexander Zacarias <113472783+Alex48821@users.noreply.github.com>
2024-03-17 15:42:03 -0500

    Created Scores class

---
+++ src/main/java/uta/cse3310/Scores.java
@@ -0,0 +1,15 @@
+package uta.cse3310;
+
+public class Scores{
+    
+    private Map<int,int> score;
+    
+    public void updateScore(){
+        //code to be implemented later
+    }
+    
+    public int getScore(){
+        //code to be implemented later
+        return 0;
+    }
+}
