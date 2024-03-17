64bb365a8520107e3a45f3dde58e1c6f08b9b186
Alexander Zacarias <113472783+Alex48821@users.noreply.github.com>
2024-03-17 15:19:46 -0500

    Added isFound & markAsFound methods

--- src/main/java/uta/cse3310/Word.java
+++ src/main/java/uta/cse3310/Word.java
@@ -3,4 +3,13 @@ package uta.cse3310;
 public class Word{
     String[] word;
     //any other necessary code to be implemented later
+
+    public boolean isFound(){
+        //code to be implemented later
+        return true;
+    }
+
+    public void markAsFound(){
+        //code to be implemented later
+    }
 }
