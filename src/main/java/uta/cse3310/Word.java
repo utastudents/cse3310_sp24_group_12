c0b27caefe0156fa38341c9d78a12de6df601c4d
Amogh <axm0754@mavs.uta.edu>
2024-03-27 16:39:59 -0500

    Added more attributes, methods

--- src/main/java/uta/cse3310/Word.java
+++ src/main/java/uta/cse3310/Word.java
@@ -1,18 +1,42 @@
 package uta.cse3310;
 
-public class Word{
-    String[] word;
-    int x;
-    int y;
-    int length;
-     //any other necessary code to be implemented later
+import java.util.Objects;
 
-    public boolean isFound(){
-        //code to be implemented later
-        return true;
+public class Word {
+    String word;
+    int startx;
+    int endx;
+    int starty;
+    int endy;
+    int length;
+
+    public Word(String word, int startx, int endx, int starty, int endy, int length) {
+        this.word = word;
+        this.startx = startx;
+        this.endx = endx;
+        this.starty = starty;
+        this.endy = endy;
+        this.length = length;
     }
 
-    public void markAsFound(){
-        //code to be implemented later
+    @Override
+    public boolean equals(Object word) {
+        if (this == word)
+            return true;
+        if (word == null || getClass() != word.getClass())
+            return false;
+        Word wordObj = (Word) word;
+
+        return (word.equals(wordObj.word) &&
+                startx == wordObj.startx &&
+                endx == wordObj.endx &&
+                starty == wordObj.starty &&
+                endy == wordObj.endy &&
+                length == wordObj.length);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(word, startx, endx, starty, endy, length);
     }
 }
