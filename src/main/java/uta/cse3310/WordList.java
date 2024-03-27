050c57bb15d7ad00aac219b5651e8b008244e8f7
Amogh <axm0754@mavs.uta.edu>
2024-03-27 15:15:21 -0500

    feat: new class

---
+++ src/main/java/uta/cse3310/WordList.java
@@ -0,0 +1,14 @@
+package uta.cse3310;
+import java.util.ArrayList;
+
+public class WordList {
+    ArrayList<String> wordList;
+
+    public void loadWord(){
+
+    }
+
+    public String getRandomWord() {
+        return "";
+    }
+}
