6a74d0b56dd5245275b8af2e0c483746bc5654f0
Amogh <axm0754@mavs.uta.edu>
2024-03-27 17:36:33 -0500

    Implement clearChat method

--- src/main/java/uta/cse3310/Chat.java
+++ src/main/java/uta/cse3310/Chat.java
@@ -1,15 +1,16 @@
 package uta.cse3310;
+
 import java.util.ArrayList;
 
-public class Chat{
+public class Chat {
     ArrayList<Message> messages;
 
-    public void sendMessage(Message message){
-        //code to be implemented later
+    public void sendMessage(Message message) {
+        // code to be implemented later
     }
 
-    public void clearChat(){
-        //code to be implemented later
+    public void clearChat() {
+        messages.clear();
     }
 
 }
