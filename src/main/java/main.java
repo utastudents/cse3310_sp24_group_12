fcf9f0618cff1d08089acc7c36d772460a518b21
Amogh <axm0754@mavs.uta.edu>
2024-04-03 04:30:06 -0500

    Add main.java file for testing purposes

---
+++ src/main/java/main.java
@@ -0,0 +1,15 @@
+import java.io.IOException;
+
+import uta.cse3310.WordList;
+
+
+
+// USE THIS MAIN FILE TO TEST YOUR CODE. THIS WILL NOT BE THERE IN OUR FINAL SUBMISSION\
+
+public class main { 
+    public static void main(String[] args) {
+        // IF YOU WNAT TO TEST A FILE IMPORT IT LIKE SHOWN IN LINE 3 AND THEN TEST YOUR FILE.
+        // This will not be there in the final submission.
+
+    }
+}
