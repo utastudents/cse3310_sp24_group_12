dff1b0c2c2922d52db2241902ff9bb48327304c3
snehabarma <126696490+snehabarma@users.noreply.github.com>
2024-04-11 18:53:58 -0500

    Functions to LoginManager

--- src/main/java/uta/cse3310/LoginManager.java
+++ src/main/java/uta/cse3310/LoginManager.java
@@ -1,24 +1,43 @@
 package uta.cse3310;
 import java.util.ArrayList;
+import java.util.List;
+
 
 public class LoginManager{
 
     private ArrayList<String> usernames;
 
-    public int registerUser(String username){
-        //code to be implemented later
-        return 0;
+    public LoginManager() {
+        this.usernames = new ArrayList<>();
     }
 
-    public ArrayList<String> getRegisteredUsers(){
-        //code to be implemented later
-        return usernames;
+
+    public boolean registerUser(String username) {
+        if (usernames.contains(username)) {
+            return false; // Username already exists
+        }
+
+        // Add the username to the list of registered users
+        usernames.add(username);
+        return true; // Successful registration
     }
 
-    public int removeUser(String username){
-        //code to be implemented later
-        return 0;
+    public List<String> getRegisteredUsers() {
+        return new ArrayList<>(usernames); // Return a copy of the list to prevent modification
     }
+
+    public boolean removeUser(String username) {
+        // Check if the username exists
+        if (!usernames.contains(username)) {
+            return false; // Username not found
+        }
+        
+        // Remove the username from the list of registered users
+        usernames.remove(username);
+        
+        
+        return true; // Successful removal
+    }
+}
 
     
-}
