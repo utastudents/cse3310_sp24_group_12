96adacca8ba76bc1b3bb6f68a3389d03e544c56a
Amogh <axm0754@mavs.uta.edu>
2024-04-15 04:31:11 -0500

    Add blank lines to UserEvent class

--- src/main/java/uta/cse3310/UserEvent.java
+++ src/main/java/uta/cse3310/UserEvent.java
@@ -2,7 +2,7 @@ package uta.cse3310;
 // User events are sent from the webpage to the server
 
 public class UserEvent {
-    int GameId; 
-    PlayerType PlayerId; 
-    int button; 
+    int GameId;
+    PlayerType PlayerId;
+    int button;
 }
