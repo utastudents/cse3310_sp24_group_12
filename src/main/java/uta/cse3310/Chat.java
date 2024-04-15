887c948de551d11926a6725390edda8835334bbb
Amogh <axm0754@mavs.uta.edu>
2024-04-15 04:26:33 -0500

    Commented code that wasn't working so I could compile

--- src/main/java/uta/cse3310/Chat.java
+++ src/main/java/uta/cse3310/Chat.java
@@ -8,20 +8,20 @@ public class Chat {
     ArrayList<String> messages = new ArrayList<String>();
 
     Scanner input = new Scanner(System.in);
-    System.out.println("<Type a message. Press enter to send.>");
-    
+    // System.out.println("<Type a message. Press enter to send.>");
+        
 
-    public void sendMessage() {
-        while (true){
-            String message = input.nextLine();
-            message = message + "\n";
-            messages.add(message);
+    // public void sendMessage() {
+    //     while (true){
+    //         String message = input.nextLine();
+    //         message = message + "\n";
+    //         messages.add(message);
 
-            for (String element : arrayList) {
-                broadcast(game);
-            }
-            messages.clear();
-        }
+    //         for (String element : arrayList) {
+    //             broadcast(game);
+    //         }
+    //         messages.clear();
+    //     }
 
     public void clearChat() {
         messages.clear();
