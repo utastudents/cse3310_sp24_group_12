0717c8e5ad09358b30f6285b80dace99171247d9
Amogh <axm0754@mavs.uta.edu>
2024-04-15 04:30:15 -0500

    Refactor LoginManager class to use Map instead of ArrayList for usernames

--- src/main/java/uta/cse3310/LoginManager.java
+++ src/main/java/uta/cse3310/LoginManager.java
@@ -1,36 +1,53 @@
 package uta.cse3310;
+
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.List;
 
 
 public class LoginManager{
     public int currentGameSize = 0;
-    private ArrayList<String> usernames;
-    public Color playerColor;
+    public Map<String, Color> usernames;
 
     public LoginManager() {
-        this.usernames = new ArrayList<>();
+        this.usernames = new HashMap<>();
     }
 
-
     public boolean registerUser(String username) {
-        if (usernames.contains(username)) {
+        if (usernames.keySet().contains(username)) {
             return false; // Username already exists
         }
 
         // Add the username to the list of registered users
-        usernames.add(username);
+        Color color;
+        if (usernames.isEmpty()) {
+            color = Color.RED; // Set the initial color to RED
+        } else {
+            // Get the largest color from the list
+            Color largestColor = Color.RED;
+            for (Color c : usernames.values()) {
+                if (c.ordinal() > largestColor.ordinal()) {
+                    largestColor = c;
+                }
+            }
+
+            // Increment the largest color by 1
+            color = Color.values()[largestColor.ordinal() + 1];
+        }
+        usernames.put(username, color);
+
         currentGameSize++;
         return true; // Successful registration
     }
 
     public List<String> getRegisteredUsers() {
-        return new ArrayList<>(usernames); // Return a copy of the list to prevent modification
+        return new ArrayList<>(usernames.keySet()); 
     }
 
     public boolean removeUser(String username) {
         // Check if the username exists
-        if (!usernames.contains(username)) {
+        if (!usernames.keySet().contains(username)) {
             return false; // Username not found
         }
         
