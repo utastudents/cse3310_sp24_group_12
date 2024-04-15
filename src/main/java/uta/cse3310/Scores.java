18384be171ff45474dbe87ca8263fdb91fba8b80
Amogh <axm0754@mavs.uta.edu>
2024-04-15 04:30:53 -0500

    Refactor Scores class to use username instead of playerId for score tracking

--- src/main/java/uta/cse3310/Scores.java
+++ src/main/java/uta/cse3310/Scores.java
@@ -5,32 +5,36 @@ import java.util.HashMap;
 
 public class Scores {
 
-    // Player id, score
-    private Map<Integer, Integer> score = new HashMap<>();
+    // username, score
+    private Map<String, Integer> score = new HashMap<>();
 
+
+    public void addNewPlayer(String username) {
+        score.put(username, 0);
+    }
     /**
      * Sets a player's score if it's a new entry. Adds to a player's score if they
      * already have an entry.
      * 
-     * @param playerId    The player's ID
+     * @param username    The player's username
      * @param playerScore The player's score
      */
-    public void updateScore(int playerId, int playerScore) {
-        Integer currentScore = score.get(playerId);
+    public void updateScore(String username, int playerScore) {
+        Integer currentScore = score.get(username);
         if (currentScore != null)
-            score.put(playerId, currentScore + playerScore);
+            score.put(username, currentScore + playerScore);
         else
-            score.put(playerId, playerScore);
+            score.put(username, playerScore);
 
     }
 
     /**
      * Fetches a player's score from the hasmap
      * 
-     * @param playerId The player's ID
+     * @param username The player's username
      * @return The player's score
      */
-    public int getScore(int playerId) {
-        return score.get(playerId);
+    public int getScore(String username) {
+        return score.get(username);
     }
 }
