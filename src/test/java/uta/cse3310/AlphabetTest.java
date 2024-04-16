cd858f55c72d958d8c7ffd84eda9185d5983db7a
Amogh <axm0754@mavs.uta.edu>
2024-04-15 21:54:35 -0500

    Add AlphabetTest class to test Alphabet class functionality

---
+++ src/test/java/uta/cse3310/AlphabetTest.java
@@ -0,0 +1,20 @@
+package uta.cse3310;
+
+import junit.framework.Assert;
+
+public class AlphabetTest {
+    public void testAlphabetFields() {
+        char letter = 'A';
+        PlayerType player = PlayerType.player_1;
+        String color = "red";
+
+        Alphabet alphabet = new Alphabet();
+        alphabet.alphabet = letter;
+        alphabet.player = player;
+        alphabet.color = color;
+
+        Assert.assertEquals(letter, alphabet.alphabet);
+        Assert.assertEquals(player, alphabet.player);
+        Assert.assertEquals(color, alphabet.color);
+    }
+}
