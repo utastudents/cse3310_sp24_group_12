205fb4b58ef70c6748778501f1923463cb91f904
lbbryant21 <100249738+lbbryant21@users.noreply.github.com>
2024-04-17 18:19:42 +0000

    new test for Chat.java

--- src/test/java/uta/cse3310/ChatTest.java
+++ src/test/java/uta/cse3310/ChatTest.java
@@ -1,1 +1,28 @@
+/* package uta.cse3310;
 
+import junit.framework.Test;
+import junit.framework.TestCase;
+import junit.framework.TestSuite;
+
+public class ChatTest 
+{
+    public ChatTest( String checkAddToChat )
+    {
+        super( checkAddToChat );
+    }
+
+    public static Test suite()
+    {
+        return new TestSuite( AppTest.class );
+    }
+
+    public void testApp()
+    {
+        newMessage = new Message();
+        newMessage.sender = "Test";
+        newMessage.message = "This is a test String";
+        chatLog.add(newMessage)
+        assertTrue( newMessage.sender.equals(chatLog[0]) && newMessage.message.equals(chatLog[1]) );
+    }
+}
+*/
