e6dfe36b096af9d3879cacf9fcd6b6b7ecb0bd01
Amogh <axm0754@mavs.uta.edu>
2024-04-17 14:56:22 -0500

    Refactor Leaderboard class to include score update and generation methods

--- src/main/java/uta/cse3310/Leaderboard.java
+++ src/main/java/uta/cse3310/Leaderboard.java
@@ -1,16 +1,28 @@
 package uta.cse3310;
 
-public class Leaderboard{
+import java.util.HashMap;
+import java.util.Map;
 
-    public void updateLeaderboard(PlayerType player, int score){
+public class Leaderboard {
+    private Map<String, Integer> score = new HashMap<>();
+
+    Leaderboard(Map<String, Integer> scores) {
+        score = scores;
         //code to be implemented later
     }
+    
 
-    public void getLeaderboard(){
-        //code to be implemented later
+    // THIS METHOD WILL BE REQUIRED IF THE SCORE IS BEING UPDATED IN REALTIME.
+    // That is, if we want to update the leaderboard immediately after a user finds a word. 
+    public void updateLeaderboard(Map<String, Integer> scores) {
+        score = scores;
     }
 
-    public void resetLeaderboard(){
-        //code to be implemented later
+    public String generateLeaderboard() {
+        // Go through each map (players username and their respective scores), sort it, generate a readable string and return it. 
+        
+        return "";
     }
+
+    // Reset leaderboard not required since we will initiate a new leaderboard every time a new game is created. 
 }
