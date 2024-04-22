71ab6fb6dc7f9abe4c8ee06f67c14979df21ebe9
Tommy1509 <122949585+Tommy1509@users.noreply.github.com>
2024-04-22 15:01:19 -0500

    Update methods

--- src/main/java/uta/cse3310/ServerEvent.java
+++ src/main/java/uta/cse3310/ServerEvent.java
@@ -3,5 +3,17 @@ package uta.cse3310;
 public class ServerEvent {
     PlayerType YouAre;
     int GameId;
-    
+
+    public Object getGameId() {
+        throw new UnsupportedOperationException("Unimplemented method 'getGameId'");
+    }
+    public Object getYouAre() {
+        throw new UnsupportedOperationException("Unimplemented method 'getYouAre'");
+    }
+    public void setYouAre(PlayerType player1) {
+        throw new UnsupportedOperationException("Unimplemented method 'setYouAre'");
+    }
+    public void setGameId(int i) {
+        throw new UnsupportedOperationException("Unimplemented method 'setGameId'");
+    }
 }
