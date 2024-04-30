9d62d65ace1db16209cff3aa78f4ecbf3d07e222
snehabarma <126696490+snehabarma@users.noreply.github.com>
2024-04-30 09:04:06 -0500

    unit test for chat

--- src/test/java/uta/cse3310/ChatTest.java
+++ src/test/java/uta/cse3310/ChatTest.java
@@ -1,29 +1,17 @@
-/* package uta.cse3310;
-
-import junit.framework.Test;
+package uta.cse3310;
 import junit.framework.TestCase;
-import junit.framework.TestSuite;
 
-public class ChatTest 
-{
-    public ChatTest( String checkAddToChat )
-    {
-        super( checkAddToChat );
-    }
+public class ChatTest extends TestCase {
 
-    public static Test suite()
-    {
-        return new TestSuite( AppTest.class );
-    }
+    public void testAddToChat() {
+        Chat chat = new Chat();
+        chat.addToChat("User1", "Hello");
+        chat.addToChat("User2", "Hi there!");
 
-    public void testApp()
-    {
-        newMessage = new Message();
-        newMessage.sender = "Test";
-        newMessage.message = "This is a test String";
-        chatLog.add(newMessage)
-        assertTrue( newMessage.sender.equals(chatLog[0]));
-        assertTrue( newMessage.message.equals(chatLog[1]) );
+        assertEquals(2, chat.chatLog.size()); // Assuming chatLog is public or has a getter method
+        assertEquals("User1", chat.chatLog.get(0).sender);
+        assertEquals("Hello", chat.chatLog.get(0).message);
+        assertEquals("User2", chat.chatLog.get(1).sender);
+        assertEquals("Hi there!", chat.chatLog.get(1).message);
     }
 }
-*/
