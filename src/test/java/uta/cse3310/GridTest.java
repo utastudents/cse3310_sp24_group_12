b2163dd726becc16b79173cb27b0f2ea1786c6ad
Amogh <axm0754@mavs.uta.edu>
2024-05-01 17:21:55 -0500

    Added deterministic word grid testing

--- src/test/java/uta/cse3310/GridTest.java
+++ src/test/java/uta/cse3310/GridTest.java
@@ -41,7 +41,6 @@ public class GridTest
     ////////////////////////////////////////////////////////////////////////////
     public void testChat() {
         Game game = new Game();
-        int messageCount = 0;
 
         game.GameId = 1;
         game.latestPlayer = PlayerType.player_1;
@@ -159,8 +158,217 @@ public class GridTest
 
         points = word.getPoints();
         for (Point p : points) {
-            assertEquals((String) game.grid.grid[p.x][p.y].color,
-                    (String) game.loginManager.usernames.get(username).name());
+                assertEquals((String) game.grid.grid[p.x][p.y].color,
+                                (String) game.loginManager.usernames.get(username).name());
+        }
+        
+        Word deterministicHorizontalWord = new Word("TESTING", 0, 0, 0, 6, 7);
+        game.grid.wordsInGrid.add(deterministicHorizontalWord);
+
+        startx = deterministicHorizontalWord.startx;
+        starty = deterministicHorizontalWord.starty;
+        endx = deterministicHorizontalWord.endx;
+        endy = deterministicHorizontalWord.endy;
+        username = "testing2";
+
+        for (int i = starty; i <= endy; i++) {
+                game.grid.grid[0][i].alphabet = deterministicHorizontalWord.word.charAt(i);
+                result = update(game, msg);
+                assertEquals((char) game.grid.grid[startx][i].alphabet,
+                                (char) deterministicHorizontalWord.word.charAt(i));
+        }
+
+        msg = String.format("{\"click\":{\"username\":\"%s\",\"x\":%d,\"y\":%d,\"color\":\"%s\"},\"gameid\":1}",
+                        username, startx, starty, game.loginManager.usernames.get(username));
+        result = update(game, msg);
+        assertEquals((String) game.grid.grid[startx][starty].color,
+                        (String) game.loginManager.usernames.get(username).name());
+
+        msg = String.format("{\"click\":{\"username\":\"%s\",\"x\":%d,\"y\":%d,\"color\":\"%s\"},\"gameid\":1}",
+                        username, endx, endy, game.loginManager.usernames.get(username));
+        result = update(game, msg);
+        assertEquals((String) game.grid.grid[endx][endy].color,
+                        (String) game.loginManager.usernames.get(username).name());
+
+        msg = String.format(
+                "{\"startClick\":{\"x\":%d,\"y\":%d},\"endClick\":{\"x\":%d,\"y\":%d},\"gameid\":1,\"color\":\"%s\",\"username\":\"%s\"}",
+                        startx, starty, endx, endy, game.loginManager.usernames.get(username), username);
+        result = update(game, msg);
+        
+        points = deterministicHorizontalWord.getPoints();
+        for (Point p : points) {
+                assertEquals((String) game.grid.grid[p.x][p.y].color,
+                                (String) game.loginManager.usernames.get(username).name());
+        }
+
+        game.grid.wordsInGrid.remove(deterministicHorizontalWord);
+        game.grid.foundWords.remove(deterministicHorizontalWord);
+
+        Word deterministicVerticalUpwardsWord = new Word("TESTING", 6, 0, 0, 0, 7);
+        game.grid.wordsInGrid.add(deterministicVerticalUpwardsWord);
+        startx = deterministicVerticalUpwardsWord.startx;
+        starty = deterministicVerticalUpwardsWord.starty;
+        endx = deterministicVerticalUpwardsWord.endx;
+        endy = deterministicVerticalUpwardsWord.endy;
+        username = "testing1";
+
+        for (int i = startx; i >= endx; i--) {
+                game.grid.grid[i][starty].alphabet = deterministicVerticalUpwardsWord.word.charAt(deterministicVerticalUpwardsWord.length - i - 1);
+                result = update(game, msg);
+                assertEquals((char) game.grid.grid[i][starty].alphabet,
+                                (char) deterministicVerticalUpwardsWord.word.charAt(
+                                                deterministicVerticalUpwardsWord.length - i - 1));
+        }
+
+        msg = String.format("{\"click\":{\"username\":\"%s\",\"x\":%d,\"y\":%d,\"color\":\"%s\"},\"gameid\":1}",
+                        username, startx, starty, game.loginManager.usernames.get(username));
+        result = update(game, msg);
+        assertEquals((String) game.grid.grid[startx][starty].color,
+                        (String) game.loginManager.usernames.get(username).name());
+
+        msg = String.format("{\"click\":{\"username\":\"%s\",\"x\":%d,\"y\":%d,\"color\":\"%s\"},\"gameid\":1}",
+                        username, endx, endy, game.loginManager.usernames.get(username));
+        result = update(game, msg);
+        assertEquals((String) game.grid.grid[endx][endy].color,
+                        (String) game.loginManager.usernames.get(username).name());
+
+        msg = String.format(
+                "{\"startClick\":{\"x\":%d,\"y\":%d},\"endClick\":{\"x\":%d,\"y\":%d},\"gameid\":1,\"color\":\"%s\",\"username\":\"%s\"}",
+                        startx, starty, endx, endy, game.loginManager.usernames.get(username), username);
+        result = update(game, msg);
+
+        points = deterministicVerticalUpwardsWord.getPoints();
+
+        for (Point p : points) {
+                assertEquals((String) game.grid.grid[p.x][p.y].color,
+                                (String) game.loginManager.usernames.get(username).name());
+        }
+
+
+        game.grid.wordsInGrid.remove(deterministicVerticalUpwardsWord);
+        game.grid.foundWords.remove(deterministicVerticalUpwardsWord);
+
+        Word deterministicVerticalDownwardsWord = new Word("TESTING", 0, 6, 0, 0, 7);
+        game.grid.wordsInGrid.add(deterministicVerticalDownwardsWord);
+        startx = deterministicVerticalDownwardsWord.startx;
+        starty = deterministicVerticalDownwardsWord.starty;
+        endx = deterministicVerticalDownwardsWord.endx;
+        endy = deterministicVerticalDownwardsWord.endy;
+        username = "testing2";
+
+        for (int i = startx; i <= endx; i++) {
+                game.grid.grid[i][starty].alphabet = deterministicVerticalDownwardsWord.word.charAt(i);
+                result = update(game, msg);
+                assertEquals((char) game.grid.grid[i][starty].alphabet,
+                                (char) deterministicVerticalDownwardsWord.word.charAt(i));
+        }
+
+        msg = String.format("{\"click\":{\"username\":\"%s\",\"x\":%d,\"y\":%d,\"color\":\"%s\"},\"gameid\":1}",
+                        username, startx, starty, game.loginManager.usernames.get(username));
+        result = update(game, msg);
+        assertEquals((String) game.grid.grid[startx][starty].color,
+                        (String) game.loginManager.usernames.get(username).name());
+
+        msg = String.format("{\"click\":{\"username\":\"%s\",\"x\":%d,\"y\":%d,\"color\":\"%s\"},\"gameid\":1}",
+                        username, endx, endy, game.loginManager.usernames.get(username));
+        result = update(game, msg);
+        assertEquals((String) game.grid.grid[endx][endy].color,
+                        (String) game.loginManager.usernames.get(username).name());
+
+        msg = String.format("{\"startClick\":{\"x\":%d,\"y\":%d},\"endClick\":{\"x\":%d,\"y\":%d},\"gameid\":1,\"color\":\"%s\",\"username\":\"%s\"}",
+                        startx, starty, endx, endy, game.loginManager.usernames.get(username), username);
+        result = update(game, msg);
+
+        points = deterministicVerticalDownwardsWord.getPoints();
+
+        for (Point p : points) {
+                assertEquals((String) game.grid.grid[p.x][p.y].color,
+                                (String) game.loginManager.usernames.get(username).name());
+        }
+
+
+        game.grid.wordsInGrid.remove(deterministicVerticalDownwardsWord);
+        game.grid.foundWords.remove(deterministicVerticalDownwardsWord);
+
+        Word deterministicDiagonalDownwardsWord = new Word("TESTING", 0, 6, 0, 6, 7);
+        game.grid.wordsInGrid.add(deterministicDiagonalDownwardsWord);
+        startx = deterministicDiagonalDownwardsWord.startx;
+        starty = deterministicDiagonalDownwardsWord.starty;
+        endx = deterministicDiagonalDownwardsWord.endx;
+        endy = deterministicDiagonalDownwardsWord.endy;
+        username = "testing1";
+
+        for (int i = startx, j = starty; i <= endx && j <= endy; i++, j++) {
+                game.grid.grid[i][j].alphabet = deterministicDiagonalDownwardsWord.word.charAt(i);
+                result = update(game, msg);
+                assertEquals((char) game.grid.grid[i][j].alphabet,
+                                (char) deterministicDiagonalDownwardsWord.word.charAt(i));
+        }
+
+        msg = String.format("{\"click\":{\"username\":\"%s\",\"x\":%d,\"y\":%d,\"color\":\"%s\"},\"gameid\":1}",
+                        username, startx, starty, game.loginManager.usernames.get(username));
+        result = update(game, msg);
+        assertEquals((String) game.grid.grid[startx][starty].color,
+                        (String) game.loginManager.usernames.get(username).name());
+
+        msg = String.format("{\"click\":{\"username\":\"%s\",\"x\":%d,\"y\":%d,\"color\":\"%s\"},\"gameid\":1}",
+                        username, endx, endy, game.loginManager.usernames.get(username));
+        result = update(game, msg);
+        assertEquals((String) game.grid.grid[endx][endy].color,
+                        (String) game.loginManager.usernames.get(username).name());
+
+        msg = String.format("{\"startClick\":{\"x\":%d,\"y\":%d},\"endClick\":{\"x\":%d,\"y\":%d},\"gameid\":1,\"color\":\"%s\",\"username\":\"%s\"}",
+                        startx, starty, endx, endy, game.loginManager.usernames.get(username), username);
+        result = update(game, msg);
+
+        points = deterministicDiagonalDownwardsWord.getPoints();
+
+        for (Point p : points) {
+                assertEquals((String) game.grid.grid[p.x][p.y].color,
+                                (String) game.loginManager.usernames.get(username).name());
+        }
+
+        game.grid.wordsInGrid.remove(deterministicDiagonalDownwardsWord);
+        game.grid.foundWords.remove(deterministicDiagonalDownwardsWord);
+
+        Word deterministicDiagonalUpwardsWord = new Word("TESTING", 6, 0, 6, 0, 7);
+        game.grid.wordsInGrid.add(deterministicDiagonalUpwardsWord);
+        startx = deterministicDiagonalUpwardsWord.startx;
+        starty = deterministicDiagonalUpwardsWord.starty;
+        endx = deterministicDiagonalUpwardsWord.endx;
+        endy = deterministicDiagonalUpwardsWord.endy;
+        username = "testing2";
+
+        for (int i = startx, j = starty; i >= endx && j >= endy; i--, j--) {
+                game.grid.grid[i][j].alphabet = deterministicDiagonalUpwardsWord.word
+                                .charAt(deterministicDiagonalUpwardsWord.length - i - 1);
+                result = update(game, msg);
+                assertEquals((char) game.grid.grid[i][j].alphabet,
+                                (char) deterministicDiagonalUpwardsWord.word
+                                                .charAt(deterministicDiagonalUpwardsWord.length - i - 1));
+        }
+
+        msg = String.format("{\"click\":{\"username\":\"%s\",\"x\":%d,\"y\":%d,\"color\":\"%s\"},\"gameid\":1}",
+                        username, startx, starty, game.loginManager.usernames.get(username));
+        result = update(game, msg);
+        assertEquals((String) game.grid.grid[startx][starty].color,
+                        (String) game.loginManager.usernames.get(username).name());
+
+        msg = String.format("{\"click\":{\"username\":\"%s\",\"x\":%d,\"y\":%d,\"color\":\"%s\"},\"gameid\":1}",
+                        username, endx, endy, game.loginManager.usernames.get(username));
+        result = update(game, msg);
+        assertEquals((String) game.grid.grid[endx][endy].color,
+                        (String) game.loginManager.usernames.get(username).name());
+
+        msg = String.format("{\"startClick\":{\"x\":%d,\"y\":%d},\"endClick\":{\"x\":%d,\"y\":%d},\"gameid\":1,\"color\":\"%s\",\"username\":\"%s\"}",
+                        startx, starty, endx, endy, game.loginManager.usernames.get(username), username);
+        result = update(game, msg);
+
+        points = deterministicDiagonalUpwardsWord.getPoints();
+
+        for (Point p : points) {
+                assertEquals((String) game.grid.grid[p.x][p.y].color,
+                                (String) game.loginManager.usernames.get(username).name());
         }
 
     }
