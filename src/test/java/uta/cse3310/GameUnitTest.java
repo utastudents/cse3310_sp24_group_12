715f3626c036058cfa4e516501a0facd0cac1f96
Amogh <axm0754@mavs.uta.edu>
2024-05-01 10:32:05 -0500

    Add testGameFields() method to GameUnitTest.java

--- src/test/java/uta/cse3310/GameUnitTest.java
+++ src/test/java/uta/cse3310/GameUnitTest.java
@@ -29,9 +29,17 @@ public class GameUnitTest
     // These are unit tests, to check methods in the Game class.
     //////////////////////////////////////////////////////////////////////////
 
-    public void testDrawOrWinner() { // first create a game object
-
+    public void testGameFields() {
+        Game game = new Game();
+        game.loginManager.registerUser("player1");
+        game.loginManager.registerUser("player2");
+        game.loginManager.registerUser("player3");
+        game.loginManager.registerUser("player4");
+        assertEquals(4, game.loginManager.currentGameSize);
+        assertEquals("player1", game.loginManager.getRegisteredUsers().get(0));
+        assertEquals("player2", game.loginManager.getRegisteredUsers().get(1));
+        assertEquals("player3", game.loginManager.getRegisteredUsers().get(2));
+        assertEquals("player4", game.loginManager.getRegisteredUsers().get(3));
     }
-
 }
 // mvn -Dtest=WholeGameTest test
