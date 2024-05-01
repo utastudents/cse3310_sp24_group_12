936b56ca8bbc52ee33c3008f66dea59d2a1b7cf5
Amogh <axm0754@mavs.uta.edu>
2024-05-01 13:57:30 -0500

    Refactor GameUnitTest.java to improve test coverage

--- src/test/java/uta/cse3310/GameUnitTest.java
+++ src/test/java/uta/cse3310/GameUnitTest.java
@@ -1,45 +1,72 @@
 package uta.cse3310;
 
-import junit.framework.Test;
-import junit.framework.TestCase;
-import junit.framework.TestSuite;
+import junit.framework.Assert;
 
-/**
- * Unit test for simple App.
- */
-public class GameUnitTest
-        extends TestCase {
-    /**
-     * Create the test case
-     *
-     * @param testName name of the test case
-     */
-    public GameUnitTest(String testName) {
-        super(testName);
+
+public class GameUnitTest {
+
+    public void testStartGame() {
+        Game game = new Game();
+        game.StartGame();
+        Assert.assertEquals(1, game.getGameState());
     }
 
-    /**
-     * @return the suite of tests being tested
-     */
-    public static Test suite() {
-        return new TestSuite(GameUnitTest.class);
+    public void testPlayer() {
+        Game game = new Game();
+        game.StartGame();
+        UserEvent userEvent = new UserEvent(); // Assuming player 1 makes a legal move at index 0
+        userEvent.PlayerId = PlayerType.player_1;
+        userEvent.button = 0;
+        userEvent.GameId = 1;
+        game.grid.grid[0][userEvent.button] = new Alphabet();
+        game.grid.grid[0][userEvent.button].player = userEvent.PlayerId;
+
+        
+        game.Update(userEvent);
+        Assert.assertEquals(PlayerType.player_2, game.currentTurn);
+        Assert.assertEquals("Player 1's move", game.msg[1]);
+        Assert.assertEquals("Your move", game.msg[0]);
+    }
+
+    public void testUpdateIllegalMove() {
+        Game game = new Game();
+        game.StartGame();
+        // Assuming player 1 tries to make an illegal move at index 0
+        UserEvent userEvent = new UserEvent();
+        userEvent.PlayerId = PlayerType.player_1;
+        userEvent.button = 0;
+        userEvent.GameId = 1;
+        game.Update(userEvent);
+        // Assert that the message reflects that it's not a legal move
+        Assert.assertEquals("Not a legal move", game.msg[0]); // Assuming player 1's index is 2
+    }
+
+    public void testAddPlayer() {
+        Game game = new Game();
+
+        game.loginManager.registerUser("player1");
+        Assert.assertEquals(1, game.loginManager.currentGameSize);
+        game.loginManager.registerUser("player2");
+        Assert.assertEquals(2, game.loginManager.currentGameSize);
+        game.loginManager.registerUser("player3");
+        Assert.assertEquals(3, game.loginManager.currentGameSize);
+        game.loginManager.registerUser("player4");
+        Assert.assertEquals(4, game.loginManager.currentGameSize);
+
+        Assert.assertEquals(4, game.loginManager.getRegisteredUsers().size());
+
     }
     
-    //////////////////////////////////////////////////////////////////////////
-    // These are unit tests, to check methods in the Game class.
-    //////////////////////////////////////////////////////////////////////////
-
-    public void testGameFields() {
+    public void testPlayerToId() {
         Game game = new Game();
         game.loginManager.registerUser("player1");
         game.loginManager.registerUser("player2");
         game.loginManager.registerUser("player3");
         game.loginManager.registerUser("player4");
-        assertEquals(4, game.loginManager.currentGameSize);
-        assertEquals("player1", game.loginManager.getRegisteredUsers().get(0));
-        assertEquals("player2", game.loginManager.getRegisteredUsers().get(1));
-        assertEquals("player3", game.loginManager.getRegisteredUsers().get(2));
-        assertEquals("player4", game.loginManager.getRegisteredUsers().get(3));
+
+        Assert.assertEquals(2, game.playerToId(PlayerType.player_1));
+        Assert.assertEquals(3, game.playerToId(PlayerType.player_2));
+        Assert.assertEquals(4, game.playerToId(PlayerType.player_3));
+        Assert.assertEquals(0, game.playerToId(PlayerType.NoPlayer));
     }
 }
-// mvn -Dtest=WholeGameTest test
