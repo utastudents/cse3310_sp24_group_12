4653d841b04096034f34eed36ff6913710cc9fc0
Amogh <axm0754@mavs.uta.edu>
2024-05-01 15:31:22 -0500

    Refactor chattest.java

--- src/test/java/uta/cse3310/ChatTest.java
+++ src/test/java/uta/cse3310/ChatTest.java
@@ -9,7 +9,6 @@ import java.lang.reflect.Array;
 import java.util.ArrayList;
 import java.util.List;
 
-
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 import com.google.gson.JsonObject;
@@ -33,7 +32,6 @@ public class ChatTest
         return new TestSuite(ChatTest.class);
     }
 
-
     private String update(Game G, String msg) {
         GsonBuilder builder = new GsonBuilder();
         Gson gson = builder.create();
@@ -42,6 +40,7 @@ public class ChatTest
         return jsonString;
     }
 
+
     ////////////////////////////////////////////////////////////////////////////
     public void testChat() {
         Game game = new Game();
@@ -49,7 +48,6 @@ public class ChatTest
 
         game.GameId = 1;
         game.latestPlayer = PlayerType.player_1;
-
         String msg = new String();
         String result = new String();
         GsonBuilder builder = new GsonBuilder();
@@ -94,59 +92,6 @@ public class ChatTest
         messageCount++;
 
         assertEquals(messageCount, game.chatLog.chatLog.size());
-
-
-
-
-        // // > 7707 1 {\"YouAre\":\"XPLAYER\",\"GameId\":1}
-        // // < 7746 *
-        // //
-        // {\"Players\":\"XPLAYER\",\"CurrentTurn\":\"NOPLAYER\",\"Button\":[\"NOPLAYER\",\"NOPLAYER\",\"NOPLAYER\",\"NOPLAYER\",\"NOPLAYER\",\"NOPLAYER\",\"NOPLAYER\",\"NOPLAYER\",\"NOPLAYER\"],\"Msg\":[\"Waiting
-        // // for other player to join\",\"\"],\"GameId\":1}
-
-        // // > 17987 2 {\"YouAre\":\"OPLAYER\",\"GameId\":1}
-        // // game.Players = PlayerType.OPLAYER;
-        // game.StartGame();
-
-        // msg =
-        // "{\"Players\":\"OPLAYER\",\"CurrentTurn\":\"XPLAYER\",\"Button\":[\"NOPLAYER\",\"NOPLAYER\",\"NOPLAYER\",\"NOPLAYER\",\"NOPLAYER\",\"NOPLAYER\",\"NOPLAYER\",\"NOPLAYER\",\"NOPLAYER\"],\"Msg\":[\"You
-        // are X. Your turn\",\"You are O. Other players turn\"],\"GameId\":1}";
-        // assertTrue(msg.indexOf("\"Msg\":[\"You are X. Your turn\"")>-1);
-
-        // result = update(game,
-        // "{\"Button\":0,\"PlayerIdx\":\"XPLAYER\",\"GameId\":1}");
-
-        // //
-        // {\"Players\":\"OPLAYER\",\"CurrentTurn\":\"OPLAYER\",\"Button\":[\"XPLAYER\",\"NOPLAYER\",\"NOPLAYER\",\"NOPLAYER\",\"NOPLAYER\",\"NOPLAYER\",\"NOPLAYER\",\"NOPLAYER\",\"NOPLAYER\"],\"Msg\":[\"Other
-
-        // // Players Move.\",\"Your Move.\"],\"GameId\":1}
-        // result =
-        // update(game,"{\"Button\":1,\"PlayerIdx\":\"OPLAYER\",\"GameId\":1}");
-        // // > 24067 *
-        // //
-        // {\"Players\":\"OPLAYER\",\"CurrentTurn\":\"XPLAYER\",\"Button\":[\"XPLAYER\",\"OPLAYER\",\"NOPLAYER\",\"NOPLAYER\",\"NOPLAYER\",\"NOPLAYER\",\"NOPLAYER\",\"NOPLAYER\",\"NOPLAYER\"],\"Msg\":[\"Your
-        // // Move.\",\"Other Players Move.\"],\"GameId\":1}
-        // result =
-        // update(game,"{\"Button\":4,\"PlayerIdx\":\"XPLAYER\",\"GameId\":1}");
-        // // > 25126 *
-        // //
-        // {\"Players\":\"OPLAYER\",\"CurrentTurn\":\"OPLAYER\",\"Button\":[\"XPLAYER\",\"OPLAYER\",\"NOPLAYER\",\"NOPLAYER\",\"XPLAYER\",\"NOPLAYER\",\"NOPLAYER\",\"NOPLAYER\",\"NOPLAYER\"],\"Msg\":[\"Other
-        // // Players Move.\",\"Your Move.\"],\"GameId\":1}
-        // result =
-        // update(game,"{\"Button\":2,\"PlayerIdx\":\"OPLAYER\",\"GameId\":1}");
-        // // > 26285 *
-        // //
-        // {\"Players\":\"OPLAYER\",\"CurrentTurn\":\"XPLAYER\",\"Button\":[\"XPLAYER\",\"OPLAYER\",\"OPLAYER\",\"NOPLAYER\",\"XPLAYER\",\"NOPLAYER\",\"NOPLAYER\",\"NOPLAYER\",\"NOPLAYER\"],\"Msg\":[\"Your
-        // // Move.\",\"Other Players Move.\"],\"GameId\":1}
-        // result =
-        // update(game,"{\"Button\":8,\"PlayerIdx\":\"XPLAYER\",\"GameId\":1}");
-
-        // assertTrue(result.indexOf("[\"You Win!\",\"You Lose!\"]")>-1);
-
-        // // > 27683 *
-        // //
-        // {\"Players\":\"OPLAYER\",\"CurrentTurn\":\"NOPLAYER\",\"Button\":[\"XPLAYER\",\"OPLAYER\",\"OPLAYER\",\"NOPLAYER\",\"XPLAYER\",\"NOPLAYER\",\"NOPLAYER\",\"NOPLAYER\",\"XPLAYER\"],\"Msg\":[\"You
-        // // Win!\",\"You Lose!\"],\"GameId\":1}
 
     }
 }
